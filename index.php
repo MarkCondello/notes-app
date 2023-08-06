6142205a3fa140219057d3d586f6062b05346184
Mark <mark.condello@sparkgreen.com.au>
2023-08-07 08:09:51 +1000

    -set up connection to DB with PDO

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 require 'functions.php';
 require 'router.php';
+
+// connect to our MySQL database.
+$dsn = 'mysql:host=localhost;port=3306;dbname=notes-app;charset=utf8';
+$pdo = new PDO($dsn, 'root', 'root');
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+echo '<pre>';
+var_dump($posts);
+echo '</pre>';
