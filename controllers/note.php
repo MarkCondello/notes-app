689aa8e6dcfe8818bcfd6907007d75261a44c17f
Mark <mark.condello@sparkgreen.com.au>
2023-08-09 21:05:54 +1000

    included authorization and status codes

--- controllers/note.php
+++ controllers/note.php
@@ -4,8 +4,16 @@ $config = require 'config.php';
 $bannerTitle = 'My note';
 $db = new Database($config['database']);
 $query = "select * from notes where id = :noteId";
-// $query = "select * from posts where id = ?";
-// dd($query);
-$note = $db->query($query, ['noteId' => $_GET['id']])->fetch();
+
+$note = $db->query($query, [
+  'noteId' => $_GET['id'],
+])->fetch();
+
+//  var_dump($note);
+if (! $note) {
+  abort();
+} else if ($note['user_id'] != 1) {
+  abort(Response::FORBIDDEN);
+}
 
 require 'views/note.view.php';
--- router.php
+++ router.php
@@ -2,7 +2,7 @@
 // DevNote: To make this router work, we needed to force all traffic to go through this index.php file with a .htaccess redirect rule.
 // DevNote: parse_url will break the uri into parts ie 'path' and 'query'.
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-function abort($code = 404){
+function abort($code = Response::NOT_FOUND){
   HTTP_response_code($code);
   require "views/{$code}.view.php";
   die();
