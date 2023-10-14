64b244e684f732fd7eafc7491b3a98f77c0ed8d6
Mark <mark.condello@sparkgreen.com.au>
2023-10-14 19:41:50 +1100

    included composer autoloader

--- public/index.php
+++ public/index.php
@@ -8,12 +8,13 @@ const BASE_PATH = __DIR__ . '/../';
 
 require BASE_PATH . 'Core/functions.php';
 
-spl_autoload_register(function ($class){
-  $class = str_replace("\\", DIRECTORY_SEPARATOR, $class);
-  // dd("{$class}.php");
-  // require basePath("Core/{$class}.php");
-  require basePath("{$class}.php");
-});
+// spl_autoload_register(function ($class){
+  //   $class = str_replace("\\", DIRECTORY_SEPARATOR, $class);
+  //   // dd("{$class}.php");
+  //   // require basePath("Core/{$class}.php");
+  //   require basePath("{$class}.php");
+  // });
+require BASE_PATH . 'vendor/autoload.php';
 
 $bootstrap = require basePath('bootstrap.php');
 
